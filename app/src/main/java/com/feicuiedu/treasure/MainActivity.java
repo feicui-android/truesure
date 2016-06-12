f4585837da5e5861e9a82a8b59715a916facb1a5
YuanChao <yuanchao@feicuiedu.com>
2016-06-12 15:05:48 +0800

    完成入口页面和登陆注册的前端搭建

---
+++ app/src/main/java/com/feicuiedu/treasure/MainActivity.java
@@ -0,0 +1,38 @@
+package com.feicuiedu.treasure;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.feicuiedu.treasure.commons.ActivityUtils;
+import com.feicuiedu.treasure.user.LoginActivity;
+import com.feicuiedu.treasure.user.RegisterActivity;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+/** 入口页面*/
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityUtils activityUtils;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityUtils = new ActivityUtils(this);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btn_Login,R.id.btn_Register})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_Login:
+                activityUtils.startActivity(LoginActivity.class);
+                break;
+            case R.id.btn_Register:
+                activityUtils.startActivity(RegisterActivity.class);
+                break;
+        }
+    }
+}
---
+++ app/src/main/java/com/feicuiedu/treasure/user/LoginActivity.java
@@ -0,0 +1,107 @@
+package com.feicuiedu.treasure.user;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.feicuiedu.treasure.HomeActivity;
+import com.feicuiedu.treasure.R;
+import com.feicuiedu.treasure.commons.ActivityUtils;
+import com.feicuiedu.treasure.commons.RegexUtils;
+import com.feicuiedu.treasure.components.AlertDialogFragment;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Bind(R.id.toolbar) Toolbar toolbar;
+    @Bind(R.id.et_Password) EditText etPassword;
+    @Bind(R.id.et_Username) EditText etUsername;
+    @Bind(R.id.btn_Login) Button btnLogin;
+
+    private ActivityUtils activityUtils; // Activity常用工具集
+
+    private String username; // 用来保存编辑框内的用户名
+    private String password; // 用来保存编辑框内的密码
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        activityUtils = new ActivityUtils(this);
+        setContentView(R.layout.activity_login);
+    }
+
+    @Override
+    public void onContentChanged() {
+        super.onContentChanged();
+        ButterKnife.bind(this);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle(getTitle());
+        etPassword.addTextChangedListener(mTextWatcher);
+        etUsername.addTextChangedListener(mTextWatcher); // EditText监听
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @OnClick(R.id.btn_Login)
+    public void login(){
+        // 正则进行判断输入的用户名是否有效
+        if (RegexUtils.verifyUsername(username) != RegexUtils.VERIFY_SUCCESS) {
+            showUsernameError();
+            return;
+        }
+        // 正则进行判断输入的密码是否有效
+        if (RegexUtils.verifyPassword(password) != RegexUtils.VERIFY_SUCCESS) {
+            showPasswordError();
+            return;
+        }
+        activityUtils.startActivity(HomeActivity.class);
+    }
+
+    private void showUsernameError() {
+        String msg = getString(R.string.username_rules);
+        AlertDialogFragment fragment = AlertDialogFragment.newInstance(R.string.username_error, msg);
+        fragment.show(getSupportFragmentManager(), "showUsernameError");
+    }
+
+    private void showPasswordError() {
+        String msg = getString(R.string.password_rules);
+        AlertDialogFragment fragment = AlertDialogFragment.newInstance(R.string.password_error, msg);
+        fragment.show(getSupportFragmentManager(), "showPasswordError");
+    }
+
+    private final TextWatcher mTextWatcher = new TextWatcher() {
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+        }
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+        }
+        @Override
+        public void afterTextChanged(Editable s) {
+            username = etUsername.getText().toString();
+            password = etPassword.getText().toString();
+            boolean canLogin = !(TextUtils.isEmpty(username) || TextUtils.isEmpty(password));
+            btnLogin.setEnabled(canLogin);
+        }
+    };
+}
