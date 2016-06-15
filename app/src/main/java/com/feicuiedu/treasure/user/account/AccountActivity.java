3688ceb426ad433a0b6fb51ca04994a557475bf7
YuanChao <yuanchao@feicuiedu.com>
2016-06-15 17:05:11 +0800

    个人用户信息内图像剪切搭建

--- app/src/main/java/com/feicuiedu/treasure/user/account/AccountActivity.java
+++ app/src/main/java/com/feicuiedu/treasure/user/account/AccountActivity.java
@@ -1,5 +1,8 @@
 package com.feicuiedu.treasure.user.account;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -9,6 +12,10 @@ import android.widget.ImageView;
 import com.feicuiedu.treasure.R;
 import com.feicuiedu.treasure.commons.ActivityUtils;
 import com.feicuiedu.treasure.components.IconSelectWindow;
+
+import org.hybridsquad.android.library.CropHandler;
+import org.hybridsquad.android.library.CropHelper;
+import org.hybridsquad.android.library.CropParams;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -39,6 +46,7 @@ public class AccountActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setTitle(getTitle());
     }
+
     /** 在当前个人用户中心页面按下icon后,弹出POPWINDOW*/
     @OnClick(R.id.iv_userIcon)
     public void onClick() {
@@ -51,17 +59,59 @@ public class AccountActivity extends AppCompatActivity {
         }
         iconSelectWindow.show();
     }
+
+    private CropHandler cropHandler = new CropHandler() {
+        @Override
+        public void onPhotoCropped(Uri uri) {
+            // 这里收到的URI就是处理完成的photo
+            // 这里等一下将来做业务逻辑
+            activityUtils.showToast("剪切成功");
+        }
+        @Override
+        public void onCropCancel() {
+            activityUtils.showToast("CropCancel");
+        }
+        @Override
+        public void onCropFailed(String message) {
+            activityUtils.showToast("CropFailed");
+        }
+        @Override
+        public CropParams getCropParams() {
+            CropParams cropParams = new CropParams();
+            cropParams.aspectX = 300;
+            cropParams.aspectY = 300;
+            return cropParams;
+        }
+        @Override
+        public Activity getContext() {
+            return AccountActivity.this;
+        }
+    };
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        // 帮助我们去处理结果(剪切完的图像)
+        CropHelper.handleResult(cropHandler, requestCode, resultCode, data);
+    }
+
     private final IconSelectWindow.Listener listener = new IconSelectWindow.Listener() {
         @Override
         public void toGallery() {
-            activityUtils.showToast("toGallery");
+            // 按下相册时
+            // 打开相册 - 到相册选择图像 - 对图像进行剪切 - 将图传回来 - 将图像上传到服务器
+            CropHelper.clearCachedCropFile(cropHandler.getCropParams().uri);
+            Intent intent = CropHelper.buildCropFromGalleryIntent(cropHandler.getCropParams());
+            startActivityForResult(intent, CropHelper.REQUEST_CROP);
         }
         @Override
         public void toCamera() {
-            activityUtils.showToast("toCamera");
+            // 按下相机时
+            // 打开相机 - 拍照 - 打开相册 - 到相册选择图像 - 对图像进行剪切 - 将图传回来 - 将图像上传到服务器
+            CropHelper.clearCachedCropFile(cropHandler.getCropParams().uri);
+            Intent intent = CropHelper.buildCaptureIntent(cropHandler.getCropParams().uri);
+            startActivityForResult(intent, CropHelper.REQUEST_CAMERA);
         }
     };
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
